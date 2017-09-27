924cf8f2cafa8364bafb01109a50f861f8c52992
Nilmar Ramos <nilmar.ramos@gmail.com>
2017-09-27 14:36:50 -0300

    commit initial

---
+++ frontend/src/main/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Messages from '../common/msg/messages'
+
+export default props => (
+	<div className="wrapper">
+		<Header/>
+		<SideBar/>
+		<div className="content-wrapper">
+			{props.children}
+		</div>
+		<Footer/>
+		<Messages/>
+	</div>
+)
